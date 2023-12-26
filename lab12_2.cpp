be60e60aa08e7c581e0243199d41e343ec3a5d56
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2023-12-26 14:29:36 +0700

    Lab12_2 Updated

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int i,int j){
+    if(i%j==0) return j;
+    else return gcd(j, i%j);
+    return 0;
 }
